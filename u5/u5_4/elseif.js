dc6efa42599e2ae3ae7fbc4ec670bc1c0f1c7e56
grace-sng7 <108954669+grace-sng7@users.noreply.github.com>
2022-09-14 12:22:12 -0500

    Create elseif.js

---
+++ u5/u5_4/elseif.js
@@ -0,0 +1,11 @@
+let password = prompt("What is your password?");
+
+if (password == "sasquatch") {
+    alert("You may enter!");
+} else if (password == "yeti") {
+    alert("Very close!");
+} else if (password == "bigfoot") {
+    alert("Close enough! You may enter.");
+} else {
+    alert("You are incorrect. You may not enter.");
+}
