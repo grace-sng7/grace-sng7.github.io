fbc1d7bf43ba584cf1940a932b3d7746dc08f274
grace-sng7 <108954669+grace-sng7@users.noreply.github.com>
2022-09-14 11:59:51 -0500

    Add files via upload

---
+++ u5/if.js
@@ -0,0 +1,18 @@
+// check if a person is 18 years or older (voting age)
+let age = 20;
+if (age >= 18) {
+    console.log("You are old enough to vote.");
+}
+console.log("Thank you for checking your age!");
+
+// check if grades are at least 90
+let compSciGrade = 95;
+if (compSciGrade >= 90) {
+    console.log("You can go to the mall.");
+}
+
+// check if a person is less than 16 years old (driving age)
+age = 14;
+if (age < 16) {
+    console.log("Sorry, you're not old enough to drive.");
+}
